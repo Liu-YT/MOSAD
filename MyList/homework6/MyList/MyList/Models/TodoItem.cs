f14730bbfe9c2a551efc0bdbdabf3aca754280cc
LiuYT <31623337+Liu-YT@users.noreply.github.com>
2018-04-18 23:41:55 +0800

    week_6

---
+++ MyList/homework6/MyList/MyList/Models/TodoItem.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+using System.ComponentModel;
+using Windows.UI.Xaml.Media;
+using System.Xml.Linq;
+using SQLite.Net.Attributes;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using System.Diagnostics;
+
+namespace MyList.Models
+{
+    public class TodoItem
+    {
+        [PrimaryKey]
+        public string id { get; set; }
+
+        public string imgName { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTimeOffset ItemDate { get; set; }
+
+        public double picSize { get; set; }
+        
+        public bool? isChecked { get; set; }
+
+        public string itemDateToString { get; set; }
+
+        [Ignore]
+        public ImageSource image { get; set; }
+
+        public async Task setImg()
+        {
+            if(imgName != null && imgName != "")
+            {
+                Debug.WriteLine(imgName);
+                var file = await ApplicationData.Current.LocalFolder.GetFileAsync(imgName);
+                IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read);
+                BitmapImage bitmapImage = new BitmapImage();
+                await bitmapImage.SetSourceAsync(fileStream);
+                this.image = bitmapImage;
+            }
+            else
+            {
+                this.image = new BitmapImage(new Uri("ms-appx:///Assets/pic_6.jpg"));
+            }
+        }
+
+        public void UpdateItem(ImageSource image, double picSize, string imgName, string title, string description, DateTimeOffset date, bool? isChecked)
+        {
+            this.image = (image == null ? new BitmapImage(new Uri("Assets/pic_5.jpg")) : image);
+            this.Title = title;
+            this.Description = description;
+            this.ItemDate = date;
+            this.picSize = picSize;
+            this.isChecked = isChecked;
+            this.imgName = imgName;
+            itemDateToString = ItemDate.ToString();
+        }
+
+        public TodoItem() { }
+
+        public TodoItem(string id, ImageSource _image, double picSize, string imgName, string title, string description, DateTimeOffset itemDate, bool? isChecked = false)
+        {
+            this.id = id;
+            this.image = ((_image == null) ? new BitmapImage(new Uri("ms-appx:///Assets/pic_6.jpg")) :_image);
+            this.picSize = picSize;
+            this.Title = title;
+            this.Description = description;
+            this.ItemDate = itemDate;
+            this.isChecked = isChecked;
+            this.imgName = imgName;
+            itemDateToString = ItemDate.ToString();
+        }
+
+        public TodoItem(TodoItem item)
+        {
+            this.id = item.id;
+            this.picSize = item.picSize;
+            this.Title = item.Title;
+            this.Description = item.Description;
+            this.ItemDate = item.ItemDate;
+            this.isChecked = item.isChecked;
+            this.imgName = item.imgName;
+            itemDateToString = ItemDate.ToString();
+        }
+    }
+}
---
+++ MyList/homework6/MyList/MyList/Service/TodoDb.cs
@@ -0,0 +1,112 @@
+﻿using SQLite.Net;
+using SQLite.Net.Platform.WinRT;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using MyList.Models;
+using System.Threading;
+using SQLite.Net.Interop;
+
+namespace MyList.Service
+{
+    public class TodoDB
+    {
+        string path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "Todos.db");
+
+        public static SQLiteConnection db;
+
+        public TodoDB()
+        {
+            try
+            {
+                db = new SQLiteConnection(new SQLitePlatformWinRT(), path);
+                db.CreateTable<TodoItem>();
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        ~TodoDB()
+        {
+            db.Close();
+        }
+
+        //获得数据库中的Item
+        public List<TodoItem> getALLItem()
+        {
+            var list = db.Table<Models.TodoItem>().ToList();
+            return list;
+        }
+        
+        //在数据库中添加Item
+        public void addItem(Models.TodoItem item)
+        {
+            try
+            {
+                Debug.WriteLine(item.itemDateToString);
+                db.Insert(item);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        //更新数据库中的Item
+        public void updateItem(Models.TodoItem item)
+        {
+            try
+            {
+                db.Update(item);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        //删除数据库中的某个Item
+        public void deleteItem(Models.TodoItem item)
+        {
+            try
+            {
+                db.Delete(item);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        //获取Item通过其id
+        public TodoItem GetItemById(string id)
+        {
+            return db.Get<TodoItem>(id);
+        }
+
+        //清空
+        public void clear()
+        {
+            db.DeleteAll<TodoItem>();
+        }
+
+        //查询
+        public List<TodoItem> queryItems(string info)
+        {
+            info = "%" + info + "%";
+            string[] _info = { info, info, info };
+            List<TodoItem> list = db.Query<TodoItem>("SELECT * FROM TodoItem WHERE (TodoItem.Title LIKE ?) OR (TodoItem.Description LIKE ?) OR (TodoItem.itemDateToString LIKE ?) ", _info); 
+            return list;
+        }
+    }
+}
